5c60d9f7e4aed65afe9a63457fc2db45c3678de3
Serhii Kushnir <s1983kushnir@gmail.com>
2024-06-18 15:27:42 +0200

    with basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  basePath: isProd ? "/reea-reveal_nextjs" : "",
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    formats: ["image/webp"],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
