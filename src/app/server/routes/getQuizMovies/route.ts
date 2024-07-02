659fe1d4f580682b8cdbc39ed5c83edb95a54daa
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-02 20:13:16 +0200

    Fix route getQuiz Movies

--- src/app/server/routes/getQuizMovies/route.ts
+++ src/app/server/routes/getQuizMovies/route.ts
@@ -1,33 +1,33 @@
 // import { quizMovies } from "../../controllers/quizMovies";
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// export const GET = async (
-//   req: NextRequest,
-//   res: NextResponse
-// ) => {
-// const { searchParams } = new URL(req.url);
-// const arrMovies = searchParams.get("strings");
-// const params = 'jou'
+export const GET = async (
+  req: NextRequest,
+  res: NextResponse
+) => {
+const { searchParams } = new URL(req.url);
+const arrMovies = searchParams.get("strings");
+const params = 'jou'
 
-//   if (req.method === "GET") {
+  if (req.method === "GET") {
       
-//       if (!params) {
+      if (!params) {
         
-//       return NextResponse.json({ error: "Movie titles are required" });
-//     }
+      return NextResponse.json({ error: "Movie titles are required" });
+    }
 
-//     const movieTitles = Array.isArray(params) ? params : [params];
+    const movieTitles = Array.isArray(params) ? params : [params];
 
-//     try {
-//         const movies = await quizMovies(arrMovies);
+    try {
+    //     const movies = await quizMovies(arrMovies);
 
-//         console.log('SERVER RESPONSE', movies)
-//       return NextResponse.json(movies);
-//     } catch (error: any) {
-//       console.error("Error fetching quiz movies:", error.message);
-//       return NextResponse.json({ error: "Failed to fetch quiz movies" });
-//     }
-//   } else {
-//     return NextResponse.json({ error: "Method not allowed" });
-//   }
-// }
+    //     console.log('SERVER RESPONSE', movies)
+    //   return NextResponse.json(movies);
+    } catch (error: any) {
+      console.error("Error fetching quiz movies:", error.message);
+      return NextResponse.json({ error: "Failed to fetch quiz movies" });
+    }
+  } else {
+    return NextResponse.json({ error: "Method not allowed" });
+  }
+}
