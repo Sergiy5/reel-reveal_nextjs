19f20f72201f6c41206ace5f2858a60c4b06bb01
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-03 12:11:09 +0200

    Chage path in getUpcomingMovies

--- src/app/api/getUpcomingMovies/route.ts
+++ src/app/api/getUpcomingMovies/route.ts
@@ -1,7 +1,13 @@
-import { fetchMovies } from "../../actions/fetchMovies";
+// import { useSearchParams } from "next/navigation";
+import { fetchMovies } from "@/app/actions/fetchMovies";
+
+
 
 export const GET = async (req: Request) => {
   if (req.method !== "GET") Response.json({ error: "Method not allowed" });
+
+  // const params = useSearchParams();
+  // const paraPAge = params?.get('page') 
 
   const { searchParams } = new URL(req.url);
   const page = searchParams.get("page");
