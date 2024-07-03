8ff532dc638a3889f2c75e1ef46f04bfba7fe1da
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-03 12:19:16 +0200

    Relocate fetchMovie

--- src/app/api/getUpcomingMovies/route.ts
+++ src/app/api/getUpcomingMovies/route.ts
@@ -1,13 +1,27 @@
-// import { useSearchParams } from "next/navigation";
-import { fetchMovies } from "@/app/actions/fetchMovies";
+import axios from "axios";
+import { Movie } from "@/types";
 
+export const fetchMovies = async (
+  category: string | undefined,
+  page: string | undefined
+): Promise<Movie[]> => {
+  
+  const apiKey = process.env.TMDB_API_KEY;
 
+  try {
+    const url = `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=${page}&api_key=${apiKey}`;
+
+    const response = await axios.get(url);
+
+    return response.data.results as Movie[];
+  } catch (error: any) {
+    console.error(`Error fetching ${category} movies:`, error.message);
+    throw error;
+  }
+};
 
 export const GET = async (req: Request) => {
   if (req.method !== "GET") Response.json({ error: "Method not allowed" });
-
-  // const params = useSearchParams();
-  // const paraPAge = params?.get('page') 
 
   const { searchParams } = new URL(req.url);
   const page = searchParams.get("page");
