6a921b3c45dcbcb23a286e78b4c32d506aa4c762
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-04 17:31:25 +0200

    change confignext

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/home",
-        permanent: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/home",
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
