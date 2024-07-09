4f74a699d19cab98f135e4250169eaea6dedda6b
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-09 15:57:22 +0200

    remove loadmorecrd

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,6 @@ export { firstMovieOnPage } from "./firstMovieOnPage";
 export { arrayFromString } from "./arrayFromString";
 export { firstElementsFromArray } from "./firstElementsFromArray";
 export { scrollToY } from "./scrollToY";
-export { loadMoreCard } from "./loadMoreCard";
 export { getFilNameFromPath } from "./getFilNameFromPath";
 export { nextQuestion } from "./nextQuestion";
 export { capitalizeFirstLetter } from "./capitalizeFirstLetter";
