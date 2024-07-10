99254dfccb130f261663b086f0cea612c631b110
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-10 21:24:44 +0200

    Added to footer max-w

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -17,41 +17,43 @@ export const Footer: React.FC<TypeDevice> = () => {
   }, []);
 
   return (
-    <footer className={`relative flex w-full justify-center items-center`}>
-      <div
-        className={`flex flex-col p-4 w-full bg-bgColor gap-8 z-20 
+    <footer className={`block mx-auto w-max`}>
+      <div className={`relative flex w-full justify-center items-center mx-auto `}>
+        <div
+          className={`flex flex-col p-4 w-full bg-bgColor gap-8 z-20 
            md:px-[60px] md:gap-6 md:py-10 md:items-start lg:px-[60px] xl:w-[1440px] xl:px-[120px]`}
-      >
-        {!isClient ? (
-          <LinkToQuiz />
-        ) : deviceType !== "mobile" ? (
-          <MainLogo />
-        ) : (
-          <LinkToQuiz />
-        )}
+        >
+          {!isClient ? (
+            <LinkToQuiz />
+          ) : deviceType !== "mobile" ? (
+            <MainLogo />
+          ) : (
+            <LinkToQuiz />
+          )}
 
-        <div className={`flex justify-between w-full  `}>
-          {!isClient ? null : deviceType !== "mobile" && <FooterMenu />}
+          <div className={`flex justify-between w-full  `}>
+            {!isClient ? null : deviceType !== "mobile" && <FooterMenu />}
 
-          <CameraIcon
-            width={154}
-            height={160}
-            id="camera-icon"
-            alt="Camera icon"
-            placeholder="empty"
-          />
+            <CameraIcon
+              width={154}
+              height={160}
+              id="camera-icon"
+              alt="Camera icon"
+              placeholder="empty"
+            />
 
-          <FooterListSocial />
+            <FooterListSocial />
+          </div>
         </div>
+        <Image
+          src={"/icons/footer_bg-ellips.svg"}
+          alt="Background ellips"
+          width={1429}
+          height={614}
+          priority
+          className={`absolute top-[100%] left-1/2 transform -translate-x-1/2 -translate-y-[100%] blur-footer z-10 w-1440 h-auto`}
+        />
       </div>
-      <Image
-        src={"/icons/footer_bg-ellips.svg"}
-        alt="Background ellips"
-        width={1429}
-        height={614}
-        priority
-        className={`absolute top-[100%] left-1/2 transform -translate-x-1/2 -translate-y-[100%] blur-footer z-10 w-1440 h-auto`}
-      />
     </footer>
   );
 };
