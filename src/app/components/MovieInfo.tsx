5cd1fea296d90f2818e8dad919ca0e52d7d8b754
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-12 20:11:27 +0200

    Fix static params error

--- src/app/components/MovieInfo.tsx
+++ src/app/components/MovieInfo.tsx
@@ -1,48 +1,49 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import Image from 'next/image'
-import { useParams} from "next/navigation"
-import { getMovieById } from '../api/actions/getMovieById'
-import { Movie } from '@/types'
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { getMovieById } from "../api/actions/getMovieById";
+import { Movie } from "@/types";
 
 export const MovieInfo: React.FC = () => {
-    const [movie, setMovie] = useState<Movie>()
-    const { id } = useParams()
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const params = useParams();
+  const { id } = params;
 
-     useEffect(() => {
-        const movieData = async (id: string | string[]) => {
-            try {
-                const result = await getMovieById(id);
-                console.log("Movie Info", result);
-                setMovie(result)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        
-        movieData(id)
-     }, [id])
-    
-    // const { poster_path } = movie;
-    // console.log('MOvie', movie)
-    const poster = `https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`;
-        // poster_path
-    //   ? `https://image.tmdb.org/t/p/w500/${movie?.poster_path}`
-    //   : "/images/no-image.jpg";
-    
-    if (!id) {
-      return <div>Loading...</div>;
+  useEffect(() => {
+    const movieData = async (id: string | string[]) => {
+      try {
+        const result = await getMovieById(id);
+        console.log("Movie Info", result);
+        setMovie(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (id) {
+      movieData(id);
     }
-    
-    return (
-      <div className={` relative w-screen max-w-[1440px] h-[800px]`} >
-            <Image
-                src={`${poster}`}
-                alt={"Movie image"}
-                width={600}
-                height={400}
-                className={` absolute -top-[120px] w-screen h-800`} />
-      </div>
-    );
-}
+  }, [id]);
+
+  if (!id || !movie) {
+    return <div>Loading...</div>;
+  }
+
+  const poster = movie.backdrop_path
+    ? `https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`
+    : "/images/no-image.jpg";
+
+  return (
+    <div className={`relative w-screen max-w-[1440px] h-[800px]`}>
+      <Image
+        src={poster}
+        alt={"Movie image"}
+        width={600}
+        height={400}
+        className={`absolute -top-[120px] w-screen h-[800px]`}
+      />
+    </div>
+  );
+};
