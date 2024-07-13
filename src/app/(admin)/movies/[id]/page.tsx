657a8c67b95d908f0ae48c8bdf9503707ef988aa
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-13 18:13:51 +0200

    Added static params

--- src/app/(admin)/movies/[id]/page.tsx
+++ src/app/(admin)/movies/[id]/page.tsx
@@ -1,11 +1,10 @@
 import { LinkToQuiz } from "@/app/components/LinkToQuiz";
 import { MovieInfo } from "@/app/components/MovieInfo";
 
-// export async function generateStaticParams() {
-// return [{ id: "533535" }];
-// }
-// { params }: { params: { id: string } }
-export default async function MoviePage() {
+export async function generateStaticParams() {
+return [{ id: "533535" }];
+}
+export default async function MoviePage({ params }: { params: { id: string } }) {
 
   return (
     <main>
