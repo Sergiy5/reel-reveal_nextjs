22f4e5d11093668eb281c7c1a6007d2daf616a51
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-15 16:39:37 +0300

    Added ternar

--- src/app/(admin)/movies/[id]/page.tsx
+++ src/app/(admin)/movies/[id]/page.tsx
@@ -6,21 +6,24 @@ import { VideoComponent } from "@/app/components/VideoComponent";
 
 export async function generateStaticParams() {
 
-  return [{ id: ""}];
+return [{ id: ""}];
 }
 export default async function MoviePage({ params }: { params: { id: string | null} }) {
 
-  const token = process.env.BEARER_TOKEN_TMDB;
+  // const token = process.env.BEARER_TOKEN_TMDB;
 
   const movie = await getMovieById(params.id);
 // console.log(movie)
-
+  const { id } = params
+  // console.log("++++++++++++++++++++++++++++++++++ID+++++++++++++++++",id)
   return (
-    <main className={`pt-0`}>
+    <>
+    {params.id ? <main className={`pt-0`}>
       <MovieInfo movie={movie} />
       {/* <VideoComponent id={params.id} /> */}
       <SliderCarousel />
       <TakeOurQuiz />
-    </main>
+    </main> : <div>Loading...</div>}
+    </>
   );
 }
