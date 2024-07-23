4703b9209a9bd9d5e3b149a70d7f35100008e248
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-23 16:19:05 +0200

    On dynamic quiz

--- src/app/(admin)/home/page.tsx
+++ src/app/(admin)/home/page.tsx
@@ -1,15 +1,22 @@
 import { Hero } from "@/app/components/Hero";
 import { HowItWorks } from "@/app/components/HowItWorks";
 import { TakeOurQuiz } from "@/app/components/TakeOurQuiz";
-import { Quiz } from "@/app/components/Quiz";
+// import { Quiz } from "@/app/components/Quiz";
 import { Genres } from "@/app/components/Genres";
 import SliderCarousel from "@/app/components/SliderCarousel";
 import { GetShowMovies } from "@/app/components/GetShowMovies";
 import { getManyMoviesByTitle, getTopRatedMovies, getUpcomingMovies } from "@/app/api/actions";
 import { firstElementsFromArray } from "@/lib";
 import { Movie } from "@/typification";
+import dynamic from "next/dynamic";
+
+const DynamicQuiz = dynamic(
+  () => import("../../components/Quiz").then((mod) => mod.Quiz),
+  { ssr: false }
+);
 
 export default async function Home() {
+  
   const topRatedMovies = await getTopRatedMovies();
   const upcomingMovies = await getUpcomingMovies();
 
@@ -21,7 +28,7 @@ export default async function Home() {
     <main>
       <Hero />
       <HowItWorks />
-      <Quiz />
+      <DynamicQuiz />
       <GetShowMovies
         title={"Upcoming 20 movies in 2024"}
         movies={upcomingMovies}
