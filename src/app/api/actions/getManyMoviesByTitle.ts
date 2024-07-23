9eda80931d167a4440a35ef652e45ab1cb690b07
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-23 16:33:03 +0200

    Remove route

--- src/app/api/actions/getManyMoviesByTitle.ts
+++ src/app/api/actions/getManyMoviesByTitle.ts
@@ -1,38 +1,42 @@
+"use server"
+
 import { Movie } from "@/typification";
 import { getTmdbUrl } from "@/lib";
 
 export const getManyMoviesByTitle = async (
   arrMovies: string[]
 ): Promise<Movie[][]> => {
-  const Back_END_URL = "/api/fetchMovies?query=";
-
-  const results = arrMovies.map((movie) => {
-    return fetch(`${Back_END_URL}${movie}`).then((res) => res.json());
-  });
-
-  const response = await Promise.all(results);
-
+  
+  
+  const TOKEN = process.env.BEARER_TOKEN_TMDB;
+  // const url = `https://api.themoviedb.org/3/search/movie?query=${searchingMovie}&language=en-US`;
+  
+  try {
+    const requests = arrMovies.map((movie) =>
+      fetch(getTmdbUrl("search", 1, movie), {
+        cache: "force-cache",
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      }).then((res) => res.json())
+    );
+  
+    const response = await Promise.all(requests);
+  
     return response.map(({ results }) => results);
+    
+  } catch (error: any) {
+    console.log("getManyMoviesByTitle error", error.message);
+    throw error;
+  }
 };
 
-// const TOKEN = process.env.BEARER_TOKEN_TMDB;
-// // const url = `https://api.themoviedb.org/3/search/movie?query=${searchingMovie}&language=en-US`;
+// const Back_END_URL = "/api/fetchMovies?query=";
 
-// try {
-//   const requests = arrMovies.map((movie) =>
-//     fetch(getTmdbUrl("search", 1, movie), {
-//       cache: "force-cache",
-//       headers: {
-//         Authorization: `Bearer ${TOKEN}`,
-//       },
-//     }).then((res) => res.json())
-//   );
+// const results = arrMovies.map((movie) => {
+//   return fetch(`${Back_END_URL}${movie}`).then((res) => res.json());
+// });
 
-//   const response = await Promise.all(requests);
+// const response = await Promise.all(results);
 
 //   return response.map(({ results }) => results);
-  
-// } catch (error: any) {
-//   console.log("getManyMoviesByTitle error", error.message);
-//   throw error;
-// }
