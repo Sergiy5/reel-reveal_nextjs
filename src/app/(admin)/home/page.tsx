69ea9570485593b44067f2cfee6a0d55e15eff64
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-23 13:03:52 +0200

    Comited all functions in pages

--- src/app/(admin)/home/page.tsx
+++ src/app/(admin)/home/page.tsx
@@ -5,25 +5,25 @@ import { TakeOurQuiz } from "@/app/components/TakeOurQuiz";
 import { Quiz } from "@/app/components/Quiz";
 import { Genres } from "@/app/components/Genres";
 import SliderCarousel from "@/app/components/SliderCarousel";
-import { getTopRatedMovies, getUpcomingMovies } from "@/app/api/actions";
+// import { getTopRatedMovies, getUpcomingMovies } from "@/app/api/actions";
 
 export default async function Home() {
-  const topRatedMovies = await getTopRatedMovies();
-  const upcomingMovies = await getUpcomingMovies();
+  // const topRatedMovies = await getTopRatedMovies();
+  // const upcomingMovies = await getUpcomingMovies();
 
-  if (!upcomingMovies) return <div>Page</div>;
-  if (!topRatedMovies) return <div>Page</div>;
+  // if (!upcomingMovies) return <div>Page</div>;
+  // if (!topRatedMovies) return <div>Page</div>;
 
   return (
     <main>
       <Hero />
       <HowItWorks />
       <Quiz />
-      <GetShowMovies
+      {/* <GetShowMovies
         title={"Upcoming 20 movies in 2024"}
         movies={upcomingMovies}
       />
-      <GetShowMovies title={"TOP 20 rated movies"} movies={topRatedMovies} />
+      <GetShowMovies title={"TOP 20 rated movies"} movies={topRatedMovies} /> */}
       <Genres />
       <SliderCarousel />
       <TakeOurQuiz />
