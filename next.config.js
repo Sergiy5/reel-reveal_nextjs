4c9dfcf73e8ce27bcf980f50e38cee62ade5b484
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-28 22:36:16 +0200

    Remove env from next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-   env: {
-    BEARER_TOKEN_TMDB:"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGU5MzBjMmZhNzRjYWVhNjRmMjAyNmNmNzBlYzQxMyIsInN1YiI6IjY0NmQwNWJhZDE4NTcyMDE4MDJlOGYyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gvC078v2ZEDlsUKlhf6XJhVHnGo7gwYMbWV5S3NVSKY"
-,
-  },
   reactStrictMode: true,
   // async redirects() {
   //   return [
