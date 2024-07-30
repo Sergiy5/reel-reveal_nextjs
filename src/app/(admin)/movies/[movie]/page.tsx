3adc84dcff7d3a500be9ffce9d1810029c9c9261
Serhii Kushnir <s1983kushnir@gmail.com>
2024-07-30 14:04:46 +0200

    Stringify obj

--- src/app/(admin)/movies/[movie]/page.tsx
+++ src/app/(admin)/movies/[movie]/page.tsx
@@ -5,6 +5,7 @@ import { MovieInfoTrailer } from "@/app/components/MovieInfoTrailer";
 import { MovieInfoCast } from "@/app/components/MovieInfoCast";
 import dynamic from "next/dynamic";
 import { getUpcomingMovies } from "@/app/services";
+import { Movie } from "@/typification";
 
 const DynamicSimilarMovies = dynamic(
   () =>
@@ -12,6 +13,10 @@ const DynamicSimilarMovies = dynamic(
   { ssr: false }
 );
 
+
+type ApiResponse = {
+  results: Movie[];
+};
 
 export async function generateStaticParams() {
   const TOKEN = process.env.BEARER_TOKEN_TMDB;
@@ -25,29 +30,52 @@ export async function generateStaticParams() {
       },
     });
 
-    const { results } = await response.json();
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
-    if (!results) return [{ movie: "3" }];
+    const data = await response.json();
 
-    return results.map((item: object) => ({movie:encodeURIComponent(JSON.stringify(item))}));
+    if (!data || !data.results) return [{ movie: "3" }];
+
+    const cutArr = data.results.slice(0, 5);
+
+    const encodedMovie = cutArr.map((item: any) => {
+// return {movie: JSON.stringify(item.id)}
+     const str = encodeURIComponent(
+        JSON.stringify({
+          title: item.title,
+          poster_path: item.poster_path,
+          vote_average: item.vote_average,
+          release_date: item.release_date,
+          runtime: item.runtime,
+          overview: item.overview,
+          backdrop_path: item.backdrop_path,
+        })
+      )
+return {movie: `${str}`}
+    });
+    // return { movie: decodeURIComponent(encodedMovie) };
+    console.log("encodedMovie+++++++++++-------------------------------------------------", encodedMovie);
+    return encodedMovie
   } catch (error) {
     console.error("Error fetching data:", error);
     return [{ movie: "3" }]; // Fallback in case of error
   }
 }
 
-
 export default async function OneMoviePage({
   params,
 }: {
-  params: { movie: string}[];
+  params: { movie: string};
   }) {
   
-  const [{ movie }] = params;
-  console.log("movie=============================================================", movie)
-  if(movie === "3") return(<div>Page</div>)
+  const { movie } = params;
+console.log("MOVIE////////////////////////////////////////////////////////////////////", movie)
+  if (movie !== "3") return (<div>Page</div>)
+  
+  const decodedMovie = JSON.parse(decodeURIComponent(movie));
 
-  const decodedMovie = JSON.parse(decodeURIComponent(movie as string));
   const { id, title, original_title } = decodedMovie;
   return (
     <main className={`pt-0 gap-0`}>
