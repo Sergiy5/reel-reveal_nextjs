773952eb5e773fed41c2c6276d971ffe5b6799bb
Serhii Kushnir <s1983kushnir@gmail.com>
2024-08-01 14:45:57 +0200

    Adde link to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <main>
       <h2>Root Page</h2>
+      <Link href={"/home"} className={`link-btn  px-12`}> Link to home page...</Link>
     </main>
   );
 }
