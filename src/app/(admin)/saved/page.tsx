6f810e45163cfbe480d26fc7f26a8a9b426636ce
Serhii Kushnir <s1983kushnir@gmail.com>
2024-09-02 19:41:49 +0200

    Remove get cookies from static page

--- src/app/(admin)/saved/page.tsx
+++ src/app/(admin)/saved/page.tsx
@@ -1,11 +1,10 @@
 import { SavedMovies } from "@/app/components/SavedMovies";
-import { cookies } from "next/headers";
 
 export default async function Saved() {
-
- const cookieStore = cookies();
- const theme = cookieStore.get("token");
-console.log("Cookies====================", theme)
-  return <main><SavedMovies />
-  </main>;
+  
+  return (
+    <main>
+      <SavedMovies />
+    </main>
+  );
 }
