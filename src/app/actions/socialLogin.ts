b8c3d4a08cb31f5a63b7192aef8818a17baac6e7
Serhii Kushnir <s1983kushnir@gmail.com>
2024-09-04 17:45:21 +0200

    To action socialLOgin/logout added string use server

--- src/app/actions/socialLogin.ts
+++ src/app/actions/socialLogin.ts
@@ -1,11 +1,12 @@
+"use server";
+
 import { signIn, signOut } from "@/auth";
 
 export async function socialLogin(formData: FormData) {
-    const action = formData.get('action') as string;
-    await signIn(action , {redirectTo: "http://localhost:3000/home"})
-    
+  const action = formData.get("action") as string;
+  await signIn(action, { redirectTo: "/home" });
 }
 
 export async function socialLogout() {
-    await signOut({redirectTo: "/home"})
+  await signOut({ redirectTo: "/home" });
 }
