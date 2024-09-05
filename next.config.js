14ed79bcfa8799f17f4e1305d51f1439d1acda8e
Serhii Kushnir <s1983kushnir@gmail.com>
2024-09-05 09:50:49 +0200

    Remove yaml

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 
 
 const nextConfig = {
+  reactStrictMode: true,
   // async redirects() {
   //   return [
   //     {
--- src/app/actions/socialLogin.ts
+++ src/app/actions/socialLogin.ts
@@ -3,7 +3,8 @@
 import { signIn, signOut } from "@/auth";
 
 export async function socialLogin(formData: FormData) {
-  const action = formData.get("action") as string;
+    const action = formData.get("action") as string;
+    
   await signIn(action, { redirectTo: "/home" });
 }
 
