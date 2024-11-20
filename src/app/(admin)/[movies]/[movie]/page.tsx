1eeb4c22af76bacb2d6dd894d3554088147a785c
Serhii Kushnir <s1983kushnir@gmail.com>
2024-11-20 22:34:28 +0100

    Add movies to dynamic params

--- src/app/(admin)/[movies]/[movie]/page.tsx
+++ src/app/(admin)/[movies]/[movie]/page.tsx
@@ -13,7 +13,7 @@ const DynamicSimilarMovies = dynamic(
 
 export async function generateStaticParams() {
  
-    return [{ movie: "3"}]; 
+    return [{ movie: "3", movies: "1" }]; 
 }
 
 export default async function OneMoviePage({
