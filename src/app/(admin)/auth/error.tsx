bf33ffc821b60b18d2ba95cc5a25f696687dff4b
Serhii Kushnir <s1983kushnir@gmail.com>
2024-11-21 08:44:58 +0100

    Puted error.tsx to all routes

---
+++ src/app/(admin)/auth/error.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { ErrorComponent, ErrorComponentProps } from "@/app/components/ui/ErrorComponent";
+
+
+export default function Error({ error, reset }: ErrorComponentProps) {
+  return <ErrorComponent error={error} reset={reset} from={"auth"} />;
+}
