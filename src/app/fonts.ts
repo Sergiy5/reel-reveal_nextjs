a1fa07fb2bdc2c067c297e2de4525c1316209901
Serhii Kushnir <s1983kushnir@gmail.com>
2025-01-22 22:53:09 +0100

    Change url to fonts remove part path ../../public

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -3,22 +3,22 @@ import localFont from "next/font/local";
 export const ceraPro = localFont({
   src: [
     {
-      path: "../../public/fonts/cera-pro/CeraPro-Light.woff2",
+      path: "/fonts/cera-pro/CeraPro-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../../public/fonts/cera-pro/CeraPro-Regular.woff2",
+      path: "/fonts/cera-pro/CeraPro-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/cera-pro/CeraPro-Medium.woff2",
+      path: "/fonts/cera-pro/CeraPro-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/fonts/cera-pro/CeraPro-Bold.woff2",
+      path: "/fonts/cera-pro/CeraPro-Bold.woff2",
       weight: "700",
       style: "normal",
     },
@@ -31,12 +31,12 @@ export const ceraPro = localFont({
 export const hind = localFont({
   src: [
     {
-      path: "../../public/fonts/hind/Hind-SemiBold.woff2",
+      path: "/fonts/hind/Hind-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../public/fonts/hind/Hind-Bold.woff2",
+      path: "/fonts/hind/Hind-Bold.woff2",
       weight: "700",
       style: "normal",
     },
