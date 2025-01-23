844ddc6b3ee7c859674bfee72ff44437bdaf5478
Serhii Kushnir <s1983kushnir@gmail.com>
2025-01-23 09:39:06 +0100

    Remove local path with public

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,52 +6,53 @@ import { ToastContainer } from "react-toastify";
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ConsentCoockie } from "./components/consentCoockie/ConsentCookie";
+
 import localFont from "next/font/local";
 
 export const urbanist = localFont({
   src: [
     {
-      path: "../../public/fonts/urbanist/Urbanist-Thin.woff2",
+      path: "/fonts/urbanist/Urbanist-Thin.woff2",
       weight: "100",
       style: "normal",
     },
     {
-      path: "../../public/fonts/urbanist/Urbanist-ExtraLight.woff2",
+      path: "/fonts/urbanist/Urbanist-ExtraLight.woff2",
       weight: "200",
       style: "normal",
     },
     {
-      path: "../../public/fonts/urbanist/Urbanist-Light.woff2",
+      path: "/fonts/urbanist/Urbanist-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../../public/fonts/urbanist/Urbanist-Regular.woff2",
+      path: "/fonts/urbanist/Urbanist-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/urbanist/Urbanist-Medium.woff2",
+      path: "/fonts/urbanist/Urbanist-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/fonts/urbanist/Urbanist-Semibold.woff2",
+      path: "/fonts/urbanist/Urbanist-Semibold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../public/fonts/urbanist/Urbanist-Bold.woff2",
+      path: "/fonts/urbanist/Urbanist-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../public/fonts/urbanist/Urbanist-ExtraBold.woff2",
+      path: "/fonts/urbanist/Urbanist-ExtraBold.woff2",
       weight: "800",
       style: "normal",
     },
     {
-      path: "../../public/fonts/urbanist/Urbanist-Black.woff2",
+      path: "/fonts/urbanist/Urbanist-Black.woff2",
       weight: "900",
       style: "normal",
     },
